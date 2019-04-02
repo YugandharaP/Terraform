bed5db970625b6eb862b5bcd7b4388064f61d2fc
Paranjape <yugandhara.paranjape@capgemini.com>
2019-04-02 14:58:13 +0530

    bash file added

--- s3-bucket-creation/new1.sh
+++ s3-bucket-creation/new1.sh
@@ -7,16 +7,16 @@ steps  {
 	terraformInstall()
 	someFunc1()
   sh '''
-	sudo apt-get install unzip
+	#sudo apt-get install unzip
 	sudo wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
 	sudo unzip terraform_0.11.13_linux_amd64.zip
 	echo $PWD
-	sudo mv terraform /var/lib/jenkins/workspace/jenks/s3-bucket-creation
-	terraform --version
-	cd /var/lib/jenkins/workspace/jenks/s3-bucket-creation
-	terraform init
-	teraform plan 
-	terraform apply --auto-approve -var access_key = ${params.access_key} secret_key = ${params.secret_key}
+	sudo mv terraform /var/lib/jenkins/workspace/jenks/s3-bucket-creation/
+	./terraform --version
+	cd /var/lib/jenkins/workspace/jenks/s3-bucket-creation/
+	./terraform init
+	./teraform plan --auto-approve -var access_key = ${params.access_key} secret_key = ${params.secret_key}
+	./terraform apply --auto-approve -var access_key = ${params.access_key} secret_key = ${params.secret_key}
   '''
    }
 }
