2af27483567a94f701671806e49a07025a887a6c
Paranjape <yugandhara.paranjape@capgemini.com>
2019-04-02 12:28:12 +0530

    bash file added

--- s3-bucket-creation/new1.sh
+++ s3-bucket-creation/new1.sh
@@ -6,18 +6,19 @@ stages {
 steps  {
 	terraformInstall()
 	someFunc1()
-  
+  sh '''
+  sudo apt-get install unzip
+	wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
+	unzip terraform_0.11.13_linux_amd64.zip
+	sudo mv terraform /usr/local/bin/
+	terraform --version
+  '''
    }
 }
 }}
 
 def terraformInstall(){
-    sudo apt-get install unzip
-	wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
-	unzip terraform_0.11.13_linux_amd64.zip
-	sudo mv terraform /usr/local/bin/
-	terraform --version
-	
+    echo "test"
 }
 
 def someFunc1(){
