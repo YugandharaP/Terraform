063adde2f5cb302a8ac41b59d10ae65e405bfd16
Paranjape <yugandhara.paranjape@capgemini.com>
2019-04-02 11:28:49 +0530

    bash file added

--- s3-bucket-creation/new1.sh
+++ s3-bucket-creation/new1.sh
@@ -1,2 +1,17 @@
-echo "terraform"
-echo ${TEST}
+pipeline{
+node {
+   stage 'test'
+   
+   sh "echo $PWD"
+   def whatThe = someFunc('textToFunc')
+}
+}
+
+def someFunc(String text){
+    echo text
+    text
+}
+def someFunc2(String text2){
+    echo text2
+    text2
+}
