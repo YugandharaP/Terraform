66be51c2c52ecc42c260bee850dc6ab066b72f0e
Paranjape <yugandhara.paranjape@capgemini.com>
2019-04-02 19:56:58 +0530

    bash file added

--- s3-bucket-creation/terraform.sh
+++ s3-bucket-creation/terraform.sh
@@ -6,13 +6,14 @@ stages {
 steps  {
 	terraformInstall()
   sh '''
-	sudo apt-get install unzip
-	sudo wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
-	sudo unzip terraform_0.11.13_linux_amd64.zip
+	#sudo apt-get install unzip
+	#sudo wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
+	#sudo unzip terraform_0.11.13_linux_amd64.zip
 	echo $PWD
-	sudo mv terraform /var/lib/jenkins/workspace/jenks/s3-bucket-creation
+	#sudo mv terraform /var/lib/jenkins/workspace/jenks/s3-bucket-creation
 	cd /var/lib/jenkins/workspace/jenks/s3-bucket-creation
 	./terraform --version
+	ls
 	./terraform init
 	./terraform plan -var access_key=${params.access_key} -var secret_key=${params.secret_key}
 	./terraform apply -auto-approve -var access_key=${params.access_key} -var secret_key=${params.secret_key}
@@ -23,4 +24,5 @@ steps  {
 
 def terraformInstall(){
     echo "terraform running on jenkins"
+	echo "${params.access_key}"
 }
