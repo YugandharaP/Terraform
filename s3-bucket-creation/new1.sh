16dfe0720dc3580a095128b2ba6f19931e74e808
Paranjape <yugandhara.paranjape@capgemini.com>
2019-04-02 12:44:10 +0530

    bash file added

--- s3-bucket-creation/new1.sh
+++ s3-bucket-creation/new1.sh
@@ -7,10 +7,11 @@ steps  {
 	terraformInstall()
 	someFunc1()
   sh '''
-  sudo -S apt-get install unzip
-	sudo -S wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
-	sudo -S unzip terraform_0.11.13_linux_amd64.zip
-	sudo -S mv terraform /usr/local/bin/
+  sudo su
+  apt-get install unzip
+	wget https://releases.hashicorp.com/terraform/0.11.13/terraform_0.11.13_linux_amd64.zip
+	unzip terraform_0.11.13_linux_amd64.zip
+	mv terraform /usr/local/bin/
 	terraform --version
   '''
    }
