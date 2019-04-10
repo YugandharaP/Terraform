b32c876c65cd4099b98db8ded596fd0f6e83abf9
yuga1007 <yugandhara.paranjape@capgemini.com>
2019-04-10 15:03:03 +0530

    added

--- terraform-modules/modules/dynamodb/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "table_name"{}
-variable "hash_key"{}
-variable "billing_mode"{}
-variable "read_capacity"{}
-variable "write_capacity"{}
-variable "type"{}
-variable "ietmsfile"{}
--- terraform-modules/modules/s3/main.tf
+++
@@ -1,7 +0,0 @@
-resource "aws_s3_bucket" "s3_bucket_creation"{
-	bucket	=	"${var.bucket_name}"
-	acl		=	"${var.acl}"
-	versioning {
-		enabled = true
-	}
-}
